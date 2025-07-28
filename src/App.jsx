c3a4e847fb418c2d22831bf4e0218e54eeda1fcd
ike-nanino <ikesemanu@gmail.com>
2025-07-28 08:37:33 -0700

    added some assets, installed dependencies and created NavBar and Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import NavBar from "./components/NavBar"
+import Hero from "./sections/Hero"
+
 
 function App() {
   return (
-    <main>
-      App
-    </main>
+    <>
+      <NavBar />
+      <Hero />
+    </>
   )
 }
 
