ec25a9510cb73c50c3b31955da02c0bb51debc39
ike-nanino <ikesemanu@gmail.com>
2025-07-30 10:41:19 -0700

    feat: implement Experience and FeatureCards sections, add GlowCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+
 import NavBar from "./components/NavBar"
+import Experience from "./sections/Experience"
+import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import LogoShowcase from "./sections/LogoShowcase"
 import AppShowcase from "./sections/ShowcaseSection"
@@ -11,7 +14,9 @@ function App() {
       <Hero />
       <AppShowcase />
       <LogoShowcase />
-    </>
+      <FeatureCards />
+      <Experience />
+      </>
   )
 }
 
