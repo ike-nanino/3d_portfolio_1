35ecd9b9bc574b5ba3646f44695103d907cdb6c8
ike-nanino <ikesemanu@gmail.com>
2025-07-30 14:36:53 -0700

    feat: add Contact and Footer sections, implement TechStack animation, and include Computer model

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
 import NavBar from "./components/NavBar"
+import Contact from "./sections/Contact"
 import Experience from "./sections/Experience"
 import FeatureCards from "./sections/FeatureCards"
+import Footer from "./sections/Footer"
 import Hero from "./sections/Hero"
 import LogoShowcase from "./sections/LogoShowcase"
 import AppShowcase from "./sections/ShowcaseSection"
+import TechStack from "./sections/TechStack"
 
 
 function App() {
@@ -16,6 +19,9 @@ function App() {
       <LogoShowcase />
       <FeatureCards />
       <Experience />
+      <TechStack />
+      <Contact />
+      <Footer />
       </>
   )
 }
